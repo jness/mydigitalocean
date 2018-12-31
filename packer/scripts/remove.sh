10664f92c3bf72f616153257e7fb4926461dc016
Jeffrey Ness <jness@atlassian.com>
2018-12-31 10:03:43 -0600

    Remove comments from scripts

--- packer/scripts/remove.sh
+++ packer/scripts/remove.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "Executing $0"
 
-# echo "---------------"
-# echo " Remove salt..."
-# echo "---------------"
-# apt-get --yes --purge remove salt-common salt-minion
-#
-# echo "--------------------------"
-# echo " Remove unused packages..."
-# echo "--------------------------"
-# apt-get --yes purge
-# apt-get --yes autoremove
-# apt-get --yes autoclean
+echo "---------------"
+echo " Remove salt..."
+echo "---------------"
+apt-get --yes --purge remove salt-common salt-minion
+
+echo "--------------------------"
+echo " Remove unused packages..."
+echo "--------------------------"
+apt-get --yes purge
+apt-get --yes autoremove
+apt-get --yes autoclean
