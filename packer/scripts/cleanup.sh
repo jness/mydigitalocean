88d6ab8773077033bbe7f95031254f5914176377
Jeffrey Ness <jness@atlassian.com>
2018-12-31 10:02:42 -0600

    Fix up nginx.conf for multi app

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -3,18 +3,18 @@ set -e
 
 echo "Executing $0"
 
-echo "------------------"
-echo " Clearing cache..."
-echo "------------------"
-rm -rf /var/lib/apt/lists/*
-
-echo "----------------------"
-echo " Cleanup after salt..."
-echo "----------------------"
-rm -rf /srv
-rm -rf /etc/salt/pki
-rm -rf /etc/salt/minion_id
-rm -rf /etc/salt/minion
-rm -rf /var/cache/salt
-rm -rf /var/log/salt
-rm -rf /tmp/salt/*
+# echo "------------------"
+# echo " Clearing cache..."
+# echo "------------------"
+# rm -rf /var/lib/apt/lists/*
+#
+# echo "----------------------"
+# echo " Cleanup after salt..."
+# echo "----------------------"
+# rm -rf /srv
+# rm -rf /etc/salt/pki
+# rm -rf /etc/salt/minion_id
+# rm -rf /etc/salt/minion
+# rm -rf /var/cache/salt
+# rm -rf /var/log/salt
+# rm -rf /tmp/salt/*
--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -3,28 +3,28 @@ set -e
 
 echo "Executing $0"
 
-echo "-------------------"
-echo " Updating ubuntu..."
-echo "-------------------"
-apt-get update
-apt-get --yes upgrade
-
-echo "---------------------------"
-echo " Installing dependencies..."
-echo "---------------------------"
-apt-get --yes install \
-  git \
-  vim \
-  wget
-
-echo "---------------------"
-echo " Install salt repo..."
-echo "---------------------"
-wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
-echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
-
-echo "-----------------------"
-echo " Install salt-minion..."
-echo "-----------------------"
-apt-get update
-apt-get --yes install salt-minion
+# echo "-------------------"
+# echo " Updating ubuntu..."
+# echo "-------------------"
+# apt-get update
+# apt-get --yes upgrade
+#
+# echo "---------------------------"
+# echo " Installing dependencies..."
+# echo "---------------------------"
+# apt-get --yes install \
+#   git \
+#   vim \
+#   wget
+#
+# echo "---------------------"
+# echo " Install salt repo..."
+# echo "---------------------"
+# wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+# echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
+#
+# echo "-----------------------"
+# echo " Install salt-minion..."
+# echo "-----------------------"
+# apt-get update
+# apt-get --yes install salt-minion
